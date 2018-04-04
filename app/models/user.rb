0764201258df853f5aee9d1bccb4127782c43faa
Sava Vuckovic <vuckovicsava11@gmail.com>
2018-04-04 09:12:45 +0200

    added pattern tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe User, type: :model do
  fixtures :users
- subject { User.new }
+ subject { users(:two) }
 
  describe 'Validations' do
    describe '#username' do
@@ -26,5 +26,34 @@ describe User, type: :model do
      end
    end
 
+   describe '#email' do
+    it 'is invalid when blank' do
+      subject.email = ' ' * 6
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid when longer than 256 characters' do
+      subject.email = 'a' * 244 + '@example.com'
+      expect(subject).to_not be_valid
+    end
+
+    it 'is valid with proper data' do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                           first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        subject.email = valid_address
+        expect(subject).to be_valid
+      end
+    end
+
+    it 'is invalid with improper data' do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        subject.email = invalid_address
+        expect(subject).to_not be_valid
+      end
+    end
+   end
  end
 end
